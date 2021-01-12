0406d5f50c1c898549759399cdd9d248f4f2bfc5
luzybry94 <luzybry94@gmail.com>
2021-01-12 15:44:44 -0500

    add detroy method and edit create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,20 +5,22 @@ class SessionsController < ApplicationController
     end
 
     def new
-      @user = User.new
+
     end
     
     def create
-      @user = User.find_by(username: params[:username])
-      if @user && @user.authenticate(params[:password])
+      @user = User.find_by(username: params[:user][:username])
+      if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
+        flash[:message] = "Invalid Credentials. Please try again."
         render :new
       end
     end
 
     def destroy
-
+      session.clear
+      redirect_to root_path
     end
 end
