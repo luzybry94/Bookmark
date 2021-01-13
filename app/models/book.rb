4ac2d3e51530eca6fc397d9dc09fa6027ac7d963
luzybry94 <luzybry94@gmail.com>
2021-01-12 19:33:18 -0500

    add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :users, through: :comments
+  validates :title, :author, :genre, :condition, :price, presence: true
 end
