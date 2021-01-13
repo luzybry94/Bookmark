42e2f299a9e3eefb988ac819387c9778a6a970a2
luzybry94 <luzybry94@gmail.com>
2021-01-13 15:20:16 -0500

    add genre association

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
+  belongs_to :genre
   has_many :comments
   has_many :users, through: :comments
   validates :title, :author, :genre, :condition, :price, presence: true
+  # accepts_nested_attributes_for :genres
 end
