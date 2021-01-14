e30c8723934b73305f362d6ba8bd43a12eabab4f
luzybry94 <luzybry94@gmail.com>
2021-01-14 16:56:39 -0500

    build genre attributes method

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,20 @@ class Book < ApplicationRecord
   has_many :comments
   has_many :users, through: :comments
   validates :title, :author, :condition, :price, presence: true
-  accepts_nested_attributes_for :genre
+  # accepts_nested_attributes_for :genre
+
+  def genre_attributes=(genre_attributes)
+     genre_attributes.values.each do |genre_attribute|
+      if genre_attribute["name"].present?
+        genre = Genre.find_or_create_by(genre_attribute)
+        self.genre << category
+      end
+    end
+  end
+
+  def self.costs_more_than(amount)
+    where("price > ?", amount)
+  end
+
+
 end
