c7cd9c8dd72d007a80213a7e6f5c8eec1776b760
luzybry94 <luzybry94@gmail.com>
2021-01-14 16:55:39 -0500

    add create action and strong params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+      @book = Book.find_by_id(params[:comment][:book_id])
+      @comment = current_user.comments.build(comment_params)
+      if @comment.save
+        redirect_to book_path(@book)
+      else
+        redirect_to book_path(@book)
+      end
+    end
+
+    def index
+
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :book_id)
+    end
 end
