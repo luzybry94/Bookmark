cae0e6c703edf702442ff64c85edda590ce5bba3
luzybry94 <luzybry94@gmail.com>
2021-01-14 09:35:52 -0500

    add accepts nested attributes macro

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ApplicationRecord
-  belongs_to :user
   belongs_to :genre
+  belongs_to :user
   has_many :comments
   has_many :users, through: :comments
-  validates :title, :author, :genre, :condition, :price, presence: true
-  # accepts_nested_attributes_for :genres
+  validates :title, :author, :condition, :price, presence: true
+  accepts_nested_attributes_for :genre
 end
