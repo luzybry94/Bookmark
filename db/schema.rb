12801f38299c69531247313f77c14ebc06927b5f
luzybry94 <luzybry94@gmail.com>
2021-01-14 09:36:52 -0500

    add genre_id to books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_144634) do
+ActiveRecord::Schema.define(version: 2021_01_14_020211) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_144634) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre_id"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
