cbf8ac4556ca057708a9cc92304f7a933b412236
luzybry94 <luzybry94@gmail.com>
2021-01-18 20:01:51 -0500

    add class methods

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,18 +6,23 @@ class Book < ApplicationRecord
   validates :title, :author, :condition, :price, presence: true
   # accepts_nested_attributes_for :genre
 
-  def genre_attributes=(genre_attributes)
-     genre_attributes.values.each do |genre_attribute|
-      if genre_attribute["name"].present?
-        genre = Genre.find_or_create_by(genre_attribute)
-        self.genre << category
-      end
-    end
+  def genre_attributes=(attributes)
+    self.genre = Genre.find_or_create_by(attributes) if !attributes['name'].empty?
+    self.genre
   end
 
   def self.costs_more_than(amount)
     where("price > ?", amount)
   end
+
+  def self.by_genre(genre_id)
+    where(genre: genre_id)
+  end
+
+  def self.recent_books
+    self.order(created_at: :desc)
+  end
+
 
 
 end
