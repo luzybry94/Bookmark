766504a9c390ccb65af9d4ca01a4b7010d0c913e
luzybry94 <luzybry94@gmail.com>
2021-01-18 19:58:11 -0500

    add link to layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,19 @@ class ApplicationController < ActionController::Base
     end
 
     def redirect_home_if_not_logged_in
-        redirect_to root_path if !logged_in?
+        if !logged_in?
+            redirect_to root_path
+        end
     end
+
+    # def redirect_home_if_not_current_user_nested
+    #     redirect_to root_path if current_user.id.to_s != params[:user_id]
+    # end
+
+    # def redirect_home_if_not_current_user
+    #     redirect_to root_path if current_user.id.to_s != params[:id]
+    # end
+
 end
+
+
