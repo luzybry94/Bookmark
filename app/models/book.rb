fb0a20ac04b29510116e65cd8451adb9b8927017
luzybry94 <luzybry94@gmail.com>
2021-01-20 11:03:10 -0500

    add image association

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :users, through: :comments
+  has_one_attached :image
   validates :title, :author, :condition, :price, presence: true
   # accepts_nested_attributes_for :genre
 
