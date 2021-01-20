c55d5568c063caf213d5d26577cccd76534be13f
luzybry94 <luzybry94@gmail.com>
2021-01-20 10:58:24 -0500

    add last crud actions and helper method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
 
     before_action :redirect_home_if_not_logged_in
+    before_action :find_book, only: [:show, :edit, :update, :destroy]
 
     def new
       if current_user.id.to_s == params[:user_id]
@@ -14,8 +15,11 @@ class BooksController < ApplicationController
     def create
       @book = current_user.books.build(book_params) 
       if @book.save
+        @book.image.purge
+        @book.image.attach(params[:book][:image])
         redirect_to books_path
       else
+        @book.build_genre
         render :new
       end
     end
@@ -29,7 +33,6 @@ class BooksController < ApplicationController
     end
 
     def show
-      @book = Book.find_by_id(params[:id])
       if @book
         @comments = @book.comments
       else
@@ -37,9 +40,34 @@ class BooksController < ApplicationController
       end
     end
 
+    def edit
+      if !current_user.books.include?(@book)
+        redirect_to user_books_path(current_user)
+      end
+    end
+
+    def update
+      if @book.update(book_params)
+        @book.image.purge
+        @book.image.attach(params[:book][:image])
+        redirect_to user_books_path(current_user)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @book.delete
+      redirect_to user_books_path(current_user)
+    end
+
     private
 
     def book_params
         params.require(:book).permit(:title, :author, :condition, :price, :genre_id, genre_attributes: [:name])
+    end
+
+    def find_book
+      @book = Book.find_by_id(params[:id])
     end
 end
