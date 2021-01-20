44b7237456c14d90500e9bdabe26f8fb26542393
luzybry94 <luzybry94@gmail.com>
2021-01-20 11:04:58 -0500

    add omniatuh class methods and edit validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,19 @@ class User < ApplicationRecord
 
     has_secure_password
     validates :email, :username, presence: true 
-    validates :email, :username, uniqueness: true 
+    validates :email, uniqueness: true 
+
+    def self.google_omniauth(auth)
+      self.find_or_create_by(email: auth[:info][:email]) do |user|
+        user.username = auth[:info][:name]
+        user.password = SecureRandom.hex
+      end
+    end
+
+    def self.github_omniauth(auth)
+      self.find_or_create_by(email: auth[:info][:email]) do |user|
+        user.username = auth[:info][:nickname]
+        user.password = SecureRandom.hex
+      end
+    end
 end
